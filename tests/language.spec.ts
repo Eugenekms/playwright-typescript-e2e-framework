267938d6286795798a8a9f0eb5aeb92a51e7d5f2
Eugene <eakvyatkovsky@gmail.com>
2026-08-04 12:29:08 +0400

    refactor: wrap tests with describe and steps, fix comments

--- tests/language.spec.ts
+++ tests/language.spec.ts
@@ -1,26 +1,45 @@
 import { test, expect } from '../fixtures/baseTest';
 test.use({ storageState: { cookies: [], origins: [] } });
 
-test('check different localization', async ({ mainPage }) => {
-    await mainPage.open();
-    await mainPage.selectLanguage('de');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Suche');
+test.describe('Localization and Language Switching', () => {
+    test('check different localization', async ({ mainPage }) => {
+        await test.step('Open main page', async () => {
+            await mainPage.open();
+        });
 
-    await mainPage.selectLanguage('el');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Αναζήτηση');
+        await test.step('Switch to German and verified placeholder', async () => {
+            await mainPage.selectLanguage('de');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Suche');
+        });
 
-    await mainPage.selectLanguage('en');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Search');
+        await test.step('Switch to Greek and verified placeholder', async () => {
+            await mainPage.selectLanguage('el');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Αναζήτηση');
+        });
 
-    await mainPage.selectLanguage('es');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Buscar');
+        await test.step('Switch to English and verified placeholder', async () => {
+            await mainPage.selectLanguage('en');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Search');
+        });
 
-    await mainPage.selectLanguage('fr');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Rechercher');
+        await test.step('Switch to Spaish and verified placeholder', async () => {
+            await mainPage.selectLanguage('es');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Buscar');
+        });
 
-    await mainPage.selectLanguage('nl');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Zoeken');
+        await test.step('Switch to French and verified placeholder', async () => {
+            await mainPage.selectLanguage('fr');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Rechercher');
+        });
 
-    await mainPage.selectLanguage('tr');
-    await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Ara');
+        await test.step('Switch to Dutch and verified placeholder', async () => {
+            await mainPage.selectLanguage('nl');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Zoeken');
+        });
+
+        await test.step('Switch to Turkish and verified placeholder', async () => {
+            await mainPage.selectLanguage('tr');
+            await expect(mainPage.searchInput).toHaveAttribute('placeholder', 'Ara');
+        });
+    });
 });
