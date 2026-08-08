1ed544a9f41495c0a2f93165605abd48511800ac
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2026-08-08 17:32:38 +0200

    fix: lint

--- tests/ui/language.spec.ts
+++ tests/ui/language.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../fixtures/baseTest';
+import { test, expect } from '@fixtures/baseTest';
 
 test.describe.skip('Localization and Language Switching', () => {
     /**
